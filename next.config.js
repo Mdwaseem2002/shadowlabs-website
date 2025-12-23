5ee549202a0ebe0fd4dd908d30298c411cc786f7
Mdwaseem2002 <waseemmd907@gmail.com>
2025-12-24 02:12:39 +0530

    Initial commit: ShadowLabs website chaneges done moblie res 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Comment out basePath for local development
-  // basePath: '/shadowlabs-website',
+  basePath: '/shadowlabs-website',
   images: {
     unoptimized: true,
   },
